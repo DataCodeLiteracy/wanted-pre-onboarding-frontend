69fd4238ecefc353da2341521e9de747e0b4b068
DataCodeLiteracy <dataliteracy@icloud.com>
2023-06-13 15:19:35 +0900

    feat: merge가 제대로 되지 않아, 수동으로 파일 수정하면서 리다이렉션 정상화

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,106 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(false);
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("access_token") || ""
+  );
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    emailRef.current = email;
+    passwordRef.current = password;
+
+    const isValidEmail = emailRef.current.indexOf("@") !== -1;
+    const isValidPassword = passwordRef.current.length >= 8;
+
+    setBtnDisabled(!isValidEmail || !isValidPassword);
+  }, [email, password]);
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/todo");
+    }
+  }, [accessToken, navigate]);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const SIGNUP_API =
+      "https://www.pre-onboarding-selection-task.shop/auth/signup";
+
+    try {
+      const req = await axios.post(
+        SIGNUP_API,
+        {
+          email,
+          password
+        },
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      window.alert("회원가입이 완료되었습니다!");
+
+      navigate("/signin");
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+
   return (
     <div>
-      <h1>회원가입 페이지 입니다.</h1>
+      <h1>회원가입</h1>
+      <form onSubmit={handleSignup}>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            id="email"
+            data-testid="email-input"
+            type="text"
+            placeholder="이메일 입력"
+            autoComplete="on"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호</label>
+          <input
+            id="password"
+            data-testid="password-input"
+            type="password"
+            placeholder="비밀번호 입력"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+
+        <button
+          data-testid="signup-button"
+          type="submit"
+          disabled={btnDisabled}
+        >
+          가입하기
+        </button>
+      </form>
     </div>
   );
 }
