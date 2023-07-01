f6eb31f214bc0eef816f8f1db31cbac997545869
DataCodeLiteracy <dataliteracy@icloud.com>
2023-07-02 03:17:58 +0900

    fix: 타입 오류 해결하기 - App, index
    * App
      - tsx로 변경
    
    * index
      - tsx로 변경
      - esModuleInterop 플래그
      - jsx 플래그
      - parsing error 해결
      - as HTMLElement
    
    * 추가로 불필요한 파일들 삭제함
    
    2023.07.02

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import App from "./App";
-import Home from "./pages/Home";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
-import Todo from "./pages/Todo";
+import App from './App'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Todo from './pages/Todo'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />
       },
       {
-        path: "signup",
+        path: 'signup',
         element: <Signup />
       },
       {
-        path: "signin",
+        path: 'signin',
         element: <Signin />
       },
       {
-        path: "todo",
+        path: 'todo',
         element: <Todo />
       }
     ]
   }
-]);
+])
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
