e6f20270f602c2d2990833d0e11091403380c05c
DataCodeLiteracy <dataliteracy@icloud.com>
2023-07-30 16:32:51 +0900

    refac: 메서드의 인자 중 accessToken 불필요해진 것 같아 제거
    * accessToken
      - accessToken을 class의 인스턴스에 전달했기 때문에, 따로 메서드에서 전달할 필요가 없어진
      것 같다.
    
    2023.07.30

--- src/api/TodoApi.ts
+++ src/api/TodoApi.ts
@@ -5,27 +5,21 @@ import localToken from './LocalToken'
 
 const api = new APIClient(REQUEST_URL, localToken.get())
 
-export const createTodo = async (accessToken: string | null, todo: string) => {
+export const createTodo = async (todo: string) => {
   return await api.post('/todos', { todo })
 }
 
-export const getTodos = async (accessToken: string | null) => {
+export const getTodos = async () => {
   return await api.get('/todos')
 }
 
-export const updateTodo = async (
-  accessToken: string | null,
-  checked: TodoType
-) => {
+export const updateTodo = async (checked: TodoType) => {
   return await api.put(`/todos/${checked.id}`, {
     todo: checked.todo,
     isCompleted: checked.isCompleted
   })
 }
 
-export const deleteTodo = async (
-  accessToken: string | null,
-  deleted: TodoType
-) => {
+export const deleteTodo = async (deleted: TodoType) => {
   return await api.delete(`/todos/${deleted.id}`)
 }
